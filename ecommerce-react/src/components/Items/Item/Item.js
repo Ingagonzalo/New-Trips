ce0f807ac4fa809730c77905143557eac54066a8
Ingagonzalo <103911185+Ingagonzalo@users.noreply.github.com>
2022-10-20 15:32:31 -0300

    Update Item.js

--- ecommerce-react/src/components/Items/Item/Item.js
+++ ecommerce-react/src/components/Items/Item/Item.js
@@ -8,7 +8,7 @@ const Item = ({ image, title, price, category, description, id }) => {
 
       <div className="card">
         <div className="imageStock">
-          <img src={image} alt="Stock" />
+          <img src={image} alt="Stock" {title} />
         </div>
 
         <div className="infoItem">
